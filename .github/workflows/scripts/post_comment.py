8d2f02d143c2dc4761767ce472bc2351f5ef11d8
kit1980-test <133920381+kit1980-test@users.noreply.github.com>
2024-10-04 17:04:59 -0700

    Update post_comment.py

--- .github/workflows/scripts/post_comment.py
+++ .github/workflows/scripts/post_comment.py
@@ -13,8 +13,7 @@ def main():
     args = parse_args()
     repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
     org, project = repo.gh_owner_and_name()
-    pr = GitHubPR(org, project, args.pr_num)
-    gh_post_pr_comment(pr.org, pr.project, pr.pr_num, "Thanks for your PR!")
+    gh_post_pr_comment(org, project, args.pr_num, "Thanks for your PR!")
 
 
 if __name__ == "__main__":
